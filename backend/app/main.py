caa0fadcd5987137a4783e2e467f41254ae3a9d9
d4phn3y <dafina.sopa@student.uni-pr.edu>
2026-06-11 16:45:59 +0200

    EVH-14 setup database and docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from app.database import engine
+from sqlalchemy import text
 
 app = FastAPI(title="EventHub API", version="1.0.0")
 
 @app.get("/")
 def read_root():
     return {"message": "Mirësevini në EventHub API!"}
+
+@app.get("/health")
+def health():
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+
+    return {
+        "status": "ok",
+        "database": "connected"
+    }
