8534a4141cd19c1321748dac906e66007850fc3f
lizesyla <lize.syla@gmail.com>
2026-06-11 16:23:14 +0200

    Add Docker support and update requirements for backend and frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+      watch: {
+        usePolling: true, // Siguron që Hot Reload të punojë saktë brenda Docker-it
+      }
+    }
 })
